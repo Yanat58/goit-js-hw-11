63302fe3031d528d5f49343d838cf90c18eb38e4
Tetiana Karlova <tanyalyt80@gmail.com>
2023-03-27 20:45:00 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -75,29 +75,6 @@ async function onSearchFormSubmit(e) {
   }
 }
 
-// async function onLoadMoreClick() {
-//   currentPage += 1;
-//   currentHits += hits.length;
-
-//   if (currentHits === totalHits) {
-//     Notify.warning(
-//       "We're sorry, but you've reached the end of search results.",
-//       {
-//         timeout: 6000,
-//       }
-//     );
-//     refs.loadMoreBtn.classList.add('is-hidden');
-//   }
-//   try {
-//     const { hits, totalHits } = await fetchGallery(searchQuery, currentPage);
-//     appendGalleryMarkup(hits);
-//     lightbox.refresh();
-//   } catch (error) {
-//     Notify.failure(error.message, 'Something went wrong!');
-//     clearGalleryMarkup();
-//   }
-// }
-
 function appendGalleryMarkup(hits) {
   refs.galleryBox.insertAdjacentHTML('beforeend', galleryMarkup(hits));
 }
